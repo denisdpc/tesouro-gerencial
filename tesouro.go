14b51cea52668de521544e8618d49f2f878c5bfb
denis <denisdpc@gmail.com>
2018-02-04 21:25:51 +0000

    identificar contrato

--- tesouro.go
+++ tesouro.go
@@ -9,8 +9,8 @@ import (
 )
 
 type Contrato struct {
-	Numero   string
 	Projeto  string
+	Numero   string
 	Empenhos map[string]*Empenho
 }
 
@@ -26,56 +26,86 @@ type Transacao struct {
 	Empenho    *Empenho
 }
 
-func relacionarProjetoContrato() []Contrato {
+func relacionarProjetoContrato() []*Contrato {
 	file, err := os.Open("contratos.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	var contratos []Contrato
+	var p_contratos []*Contrato
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		aux := strings.Split(scanner.Text(), ":")
-		contratos = append(contratos, Contrato{Numero: aux[0], Projeto: aux[1]})
+		p_contratos = append(p_contratos, &Contrato{Projeto: aux[0], Numero: aux[1]})
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	return contratos
+	return p_contratos
+}
+
+func identificarContrato(contratos []*Contrato, obs string) int {
+	for i, cnt := range contratos {
+		if strings.Contains(obs, cnt.Numero) {
+			fmt.Println(cnt.Numero)
+			return i
+		}
+	}
+	return -1
 }
 
 func main() {
 
-	contratos := relacionarProjetoContrato()
-	fmt.Println(contratos)
+	p_contratos := relacionarProjetoContrato()
+	fmt.Println(p_contratos)
 
-	// LEITURA DO ARQUIVO
+	linha := []string{"NE80032", "xxxx 031/GAL-PAMASP/2016 DSFF"}
+	//obs := "xxxx 046/GAL-PAMASP/2017 DSFF"
+	j := identificarContrato(p_contratos, linha[1])
+	fmt.Println(j) // se j == -1 , contrato nao esta presente na observacao
+
+	/*
+		for i, _ := range contratos {
+			cnt := contratos[i]
+			fmt.Println(i, cnt)
+		}
+
+		aux := [2]string{"asas", "dfdf"}
+		identificarContrato(contratos, aux)
+
+		for i, _ := range contratos {
+			cnt := contratos[i]
+			fmt.Println(i, cnt)
+		}
+	*/
+
+	// LEITURA DO ARQUIVOs
 	// CRIAR EMPENHO CONFORME LEITURA
 	// VERIFICAR SE EMPENHO TEM CONTRATO ASSOCIADO
 	// SE NAO TIVER, VERIFICAR SE NA OBSERVAcao CONSTA ALGUM DOS CONTRATOS PARA ASSOCIAR
 	// RELACIONAR TRANSACAO AO EMPENHO
 
 	// TESTES
+	/*
+		e11 := Empenho{Numero: "NE800321"}
+		c1 := Contrato{Numero: "001/PAMASP/2011"}
 
-	e11 := Empenho{Numero: "NE800321"}
-	c1 := Contrato{Numero: "001/PAMASP/2011"}
+		e11.Contrato = &c1
 
-	e11.Contrato = &c1
+		c1.Empenhos = make(map[string]*Empenho)
+		c1.Empenhos[e11.Numero] = &e11
 
-	c1.Empenhos = make(map[string]*Empenho)
-	c1.Empenhos[e11.Numero] = &e11
+		fmt.Println(c1.Numero, (*e11.Contrato).Numero)
 
-	fmt.Println(c1.Numero, (*e11.Contrato).Numero)
+		c1.Numero = "002/PAMASP"
 
-	c1.Numero = "002/PAMASP"
+		fmt.Println(c1.Numero, (*e11.Contrato).Numero)
 
-	fmt.Println(c1.Numero, (*e11.Contrato).Numero)
+		e11.Numero = "NE12344"
 
-	e11.Numero = "NE12344"
-
-	fmt.Println(c1.Empenhos["NE800321"].Numero)
-
+		fmt.Println(c1.Empenhos["NE800321"].Numero)
+	*/
 }
